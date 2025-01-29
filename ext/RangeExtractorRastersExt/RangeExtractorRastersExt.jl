ecf64f0b20483742e34d685666d67760c4615f61
Anshul Singhvi <anshulsinghvi@gmail.com>
2025-01-29 11:45:31 -0500

    Fix typo

--- ext/RangeExtractorRastersExt/RangeExtractorRastersExt.jl
+++ ext/RangeExtractorRastersExt/RangeExtractorRastersExt.jl
@@ -3,9 +3,16 @@ module RangeExtractorRastersExt
 using RangeExtractor
 using Rasters
 
-function similar_blank(array::AbstractRaster)
+function RangeExtractor.similar_blank(array::AbstractRaster)
+    # Construct a new array of the same type as `array`,
+    # backed by unclean memory.
     new = similar(array)
-    new.data .= Rasters.missingval(array)
+
+    # If the array doesn't have a missing value,
+    # we can just fill it with zeros.
+    fillval = isnothing(Rasters.missingval(array)) ? zero(eltype(array)) : Rasters.missingval(array)
+    # Fill the new array with the fill value.   
+    fill!(new, fillval)
     return new
 end
 
